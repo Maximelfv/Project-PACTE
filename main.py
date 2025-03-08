67462086641e041f38e9c6d803da99f6d7b76ce5
Maximelfv <maxime.lefeuvre@etu.estia.fr>
2025-03-08 19:04:18 +0100

    premier ecrit de prog

---
+++ main.py
@@ -0,0 +1,13 @@
+from dash import Dash, html
+
+from src.components.layout import create_layout
+
+def main() -> None:
+    app = Dash()
+    app.title = "Medal"
+    app.layout = create_layout(app)
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
